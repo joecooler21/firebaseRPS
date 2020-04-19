94fd029c1c24d4ad75b48fbc1b59bd44e33efa71
unknown <joe_cooler@msn.com>
2020-04-19 10:47:39 -0500

    finished ui layout, styling and began basic game logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,25 @@
+var config = {
+
+    apiKey: "AIzaSyDitAXjuCRaclQJVq-u8Lj5hXKu376wo0Y",
+    authDomain: "wisc-lc-cd1c2.firebaseapp.com",
+    databaseURL: "https://wisc-lc-cd1c2.firebaseio.com",
+    projectId: "wisc-lc-cd1c2",
+    storageBucket: "wisc-lc-cd1c2.appspot.com",
+  };
+
+  var rockButton = document.getElementById("rock");
+  var paperButton = document.getElementById("paper");
+  var scissorsButton = document.getElementById("scissors");
+
+  rockButton.addEventListener('click', function () {
+  });
+
+  paperButton.addEventListener('click', function () {
+  });
+
+  scissorsButton.addEventListener('click', function () {
+  });
+
 function appendChat(text) {
     var chat = document.getElementById("chat-box");
     var ele = document.createElement("div");
@@ -5,3 +27,6 @@ function appendChat(text) {
     ele.textContent = "Player 1: " + text;
     chat.appendChild(ele);
 }
+
+firebase.initializeApp(config);
+var database = firebase.database();
