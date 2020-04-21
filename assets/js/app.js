19e76162c28006c21134eb1c923c48b74ad2f851
unknown <joe_cooler@msn.com>
2020-04-21 15:26:25 -0500

    added firebase functionality and basic game logic completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,47 @@ var config = {
   storageBucket: "rps-project-6dc82.appspot.com",
 };
 
+var playerID = 0;
+var opp = null;
+var ref = null;
+
 firebase.initializeApp(config);
 var database = firebase.database();
 
-resetAll();
+// connect to database and see if any children, then assign playerID and set initial game variables
+database.ref().once('value').then(function(snap) {
 
-var gameState = false;
+  // 1 child per player, set ID's accordingly
+  if (snap.numChildren() === 0) {
+    playerID = 1;
+    opp = "player2";
+  } else if (snap.numChildren() === 1) {
+    playerID = 2;
+    opp = "player1";
+  } 
+  // set variables for game logic
+}).then(function(){
+  database.ref().child('player' + playerID).set({
+    move: 0,
+    wins: 0,
+    chat: 0
+
+  })
+}).then(function(){
+  // set a listener for move of each player
+  database.ref('/player' + playerID + '/move').on('value', function(snap){
+    if (snap.val() === 0) {
+      return;
+    }
+    //console.log(snap.val());
+  })
+  
+}).then(function(){
+  // remove player child on disconnect
+  database.ref('player' + playerID).onDisconnect().remove()
+}).then(function() {
+  console.log("You are player" + playerID + " and your opponent is " + opp);
+})
 
 var rockButton = document.getElementById("rock");
 var paperButton = document.getElementById("paper");
@@ -38,11 +73,8 @@ rockButton.addEventListener('click', function () {
     statusWindow.style.visibility = 'visible';
     statusWindow.textContent = "WAITING FOR PLAYER...";
   }
-  if (!gameState) {
-    setGameState(true);
-  }
-  statusWindow.textContent = "PLAYER CHOOSES ROCK";
-  database.ref('/p1_move').set("rock");
+  statusWindow.textContent = "PLAYER " + playerID + " CHOOSES ROCK";
+  database.ref('/player' + playerID + '/move').set("rock").then(result());
 });
 
 paperButton.addEventListener('click', function () {
@@ -51,11 +83,8 @@ paperButton.addEventListener('click', function () {
     statusWindow.style.visibility = 'visible';
     statusWindow.textContent = "WAITING FOR PLAYER...";
   }
-  if (!gameState) {
-    setGameState(true);
-  }
-  statusWindow.textContent = "PLAYER CHOOSES PAPER";
-  database.ref('/p1_move').set("paper");
+  statusWindow.textContent = "PLAYER " + playerID + " CHOOSES PAPER";
+  database.ref('/player' + playerID + '/move').set("paper").then(result());
 });
 
 scissorsButton.addEventListener('click', function () {
@@ -64,23 +93,9 @@ scissorsButton.addEventListener('click', function () {
     statusWindow.style.visibility = 'visible';
     statusWindow.textContent = "WAITING FOR PLAYER...";
   }
-  if (!gameState) {
-    setGameState(true);
-  }
-  statusWindow.textContent = "PLAYER CHOSES SCISSORS";
-  database.ref('/p1_move').set("scissors");
+  statusWindow.textContent = "PLAYER " + playerID + " CHOOSES SCISSORS";
+  database.ref('/player' + playerID + '/move').set("scissors").then(result());
 });
-
-
-// change gameState flag to the value in database
-database.ref('/game_state').on('value', function (snap) {
-  gameState = snap.val();
-
-});
-
-function setGameState (state) {
-  database.ref('/game_state').set(state);
-}
 
 function appendChat(text) {
   var chat = document.getElementById("chat-box");
@@ -90,12 +105,51 @@ function appendChat(text) {
   chat.appendChild(ele);
 }
 
-function resetAll() {
-  database.ref().set({
-    current_player:0,
-    game_state:false,
-    p1_move:0,
-    p2_move:0,
-    current_player:0
+function result() {
+  var p1_move, p2_move;
+  database.ref().once('value', function(snap) {
+    p1_move = snap.val().player1.move;
+    p2_move = snap.val().player2.move;
+  }).then(function() {
+    if(p1_move === p2_move) {
+      console.log("tie");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "rock" && p2_move === "paper") {
+      console.log("player 2 wins");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "rock" && p2_move === "scissors") {
+      console.log("player 1 wins");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "paper" && p2_move === "rock") {
+      console.log("player 1 wins");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "paper" && p2_move === "scissors") {
+      console.log("player 2 wins");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "scissors" && p2_move === "rock") {
+      console.log("player 2 wins");
+      resetMoves()
+      return;
+    }
+    if (p1_move === "scissors" && p2_move === "paper") {
+      console.log("player 1 wins");
+      resetMoves()
+      return;
+    }
   });
+}
+
+function resetMoves() {
+  database.ref('player1/move').set('0');
+  database.ref('player2/move').set('0');
 }
